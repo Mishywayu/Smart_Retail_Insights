4c89f979c2e46199a084bc2edbba10cdaf58505e
Mishywayu <michellewayua2@gmail.com>
2025-03-18 18:35:32 +0300

    Building a model  ...ongoing

--- customer_level.py
+++ customer_level.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 df1 = pd.read_csv("customer_level_dataset.csv")
 
@@ -21,6 +23,23 @@ X_test_scaled = scaler.transform(X_test)  # Only transform test data (no fitting
 print("Feature Scaling Completed!")
 
 
+
+# Building a model
+#initialize the model
+model = LinearRegression()
+
+# train the model
+model.fit(X_train_scaled, y_train)
+
+# make predictions
+y_pred = model.predict(X_test_scaled)
+
+# check how good it is
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"R² Score: {r2:.2f}")
 
 # print(df1.info())
 # print(df1.head())
